651ea082bd2b68070c4189487c0f3bb338c13736
Raphael Berger <raphaelmb@gmail.com>
2025-07-08 15:56:29 -0300

    new: add list rooms endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import { fastify } from "fastify"
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
 import { fastifyCors } from "@fastify/cors"
 import { env } from "./env.ts"
+import { getRoomsRoute } from "./http/routes/get-rooms.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,6 +16,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.get("/health", () => {
     return "OK"
 })
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT })
     .then(() => console.log(`HTTP server running ${env.PORT}`))
