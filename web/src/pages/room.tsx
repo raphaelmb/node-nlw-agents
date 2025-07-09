e3f6ca5134431d84968b70206e8d05eeccbdcefd
Raphael Berger <raphaelmb@gmail.com>
2025-07-09 09:23:55 -0300

    frontend setup

---
+++ web/src/pages/room.tsx
@@ -0,0 +1,13 @@
+import { Navigate, useParams } from "react-router-dom"
+
+type RoomParams = {
+    roomId: string
+}
+
+export function Room() {
+    const params = useParams<RoomParams>()
+    
+    if (!params.roomId) return <Navigate to="/" replace />
+
+    return <div>Room Details</div>
+}
